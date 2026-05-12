c585a3e62b2544010620d9ef9990a71933d65381
Lokendra Rokaya <rokayalokendra0@gmail.com>
2026-05-12 19:02:12 +0545

    dashboard created sucessfully

--- RoomMap/models.py
+++ RoomMap/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Signup(models.Model):
+    fullname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
--- RoomMap/views.py
+++ RoomMap/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Signup
 
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
+def home(request):
+
+    if request.method == "POST":
+
+        # SIGNUP
+        if 'signup_username' in request.POST:
+
+            fullname = request.POST.get('fullname')
+            username = request.POST.get('signup_username')
+            email = request.POST.get('email')
+            password = request.POST.get('signup_password')
+
+            data = Signup(
+                fullname=fullname,
+                username=username,
+                email=email,
+                password=password
+            )
+
+            data.save()
+
+            return redirect('/')
+
+        # LOGIN
+        elif 'login_username' in request.POST:
+
+            username = request.POST.get('login_username')
+            password = request.POST.get('login_password')
+
+            user = Signup.objects.filter(
+                username=username,
+                password=password
+            )
+
+            if user.exists():
+                return redirect('/dashboard/')
+
+    return render(request, 'index.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
